d5399c80518abe3e06e43a9a7dececb10ad58cdb
Emily Crandall Fleischman <emilycf@mit.edu>
2018-07-19 09:19:29 -0700

    Better error printing in main program

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,35 @@ mod input;
 mod parse;
 
 use std::io;
+use failure::Fail;
 
-fn main() -> errors::Result<()> {
+fn main() {
     // for now, just read from stdin
     let stdin = io::stdin();
     let mut input = input::Input::new(stdin.lock());
     loop {
-        let mut block = input.next_block()?;
+        let mut block = match input.next_block() {
+            Err(e) => {
+                print_errors(e);
+                continue;
+            }
+            Ok(block) => block,
+        };
         if block.len() == 0 {
-            return Ok(());
+            return;
         }
-        println!("{:?}", block.parse()?);
+        match block.parse() {
+            Err(e) => {
+                print_errors(e);
+                continue;
+            }
+            Ok(block) => println!("{:?}", block),
+        }
+    }
+}
+
+fn print_errors(e: impl Fail) {
+    for c in e.causes() {
+        println!("{}", c);
     }
 }
