33b5e2198ee009e9cea49a27543545999027e559
Emily Crandall Fleischman <emilycf@mit.edu>
2018-07-18 20:17:11 -0700

    Add skeleton main program

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,17 @@ mod errors;
 mod input;
 mod parse;
 
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> errors::Result<()> {
+    // for now, just read from stdin
+    let stdin = io::stdin();
+    let mut input = input::Input::new(stdin.lock());
+    loop {
+        let mut block = input.next_block()?;
+        if block.len() == 0 {
+            return Ok(());
+        }
+        println!("{:?}", block.parse()?);
+    }
 }
